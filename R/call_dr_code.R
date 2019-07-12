0d83217209b65127756695b49a7fd0ac1dd5728f
Patrick English <39728632+patrick-eng@users.noreply.github.com>
2019-07-12 15:41:01 +0100

    add call function

---
+++ R/call_dr_code.R
@@ -0,0 +1,17 @@
+#' Call and Store Dyad-Ratios Functions
+#'
+#' Collects and assigns functions required to run the Stimson dyad-ratios model.
+#'
+#' @param filepath Character vector indicating the location and filename of the dyad-ratios functions. Defula is load_function.R
+#'
+#' @details Simple function to source and assign all dyad-ratios model functions into the global environment.
+#'
+#' Takes filepath argument, which by default looks in the same folder for load_functions.R.
+#'
+#' Sigificant changes have been made to the extract function (non-calculation elements) which are documented in the main package documentation.
+
+call_dr_code <- function(filepath="load_functions.R"){
+
+  source(filepath)
+
+}
